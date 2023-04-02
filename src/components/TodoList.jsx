6ba6d2ec66d2bf2fb28ae92220f52ad7a8993358
JKyEun <j56237@naver.com>
2023-04-02 16:52:21 +0900

    feat: todoList 목록 보는 기능, 새로운 todo 추가 기능, 체크박스 체크 기능, 삭제 기능 구현

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,85 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
-export default function TodoList({ todo }) {
+export default function TodoList({ id, todo, isCompleted, setTodos, todos }) {
+  const checkBox = useRef();
+  const todoSpan = useRef();
+  const [isChecked, setIsChecked] = useState(isCompleted);
+
+  const updateCheckBox = async () => {
+    const todo = {
+      todo: todoSpan.current.innerText,
+      isCompleted: !isChecked,
+    };
+
+    const accessToken = JSON.parse(localStorage.getItem('JWT')).access_token;
+
+    try {
+      const res = await fetch(
+        `https://pre-onboarding-selection-task.shop/todos/${id}`,
+        {
+          method: 'PUT',
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(todo),
+        }
+      );
+
+      if (res.status === 200) {
+        const data = await res.json();
+        console.log('성공', data);
+        setIsChecked(data.isCompleted);
+      } else {
+        console.log(`요청실패, status는 ${res.status}`);
+      }
+    } catch (err) {
+      console.error(`요청실패, 에러는 ${err}`);
+    }
+  };
+
+  const deleteTodo = async () => {
+    const accessToken = JSON.parse(localStorage.getItem('JWT')).access_token;
+
+    try {
+      const res = await fetch(
+        `https://pre-onboarding-selection-task.shop/todos/${id}`,
+        {
+          method: 'DELETE',
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      if (res.status === 204) {
+        console.log('성공');
+        const newTodo = todos.filter((todo) => todo.id !== id);
+        setTodos(newTodo);
+      } else {
+        console.log(`요청실패, status는 ${res.status}`);
+      }
+    } catch (err) {
+      console.error(`요청실패, 에러는 ${err}`);
+    }
+  };
+
   return (
     <>
       <li>
         <label>
-          <input type='checkbox' checked={todo.isChecked} />
-          <span>{todo.content}</span>
+          <input
+            ref={checkBox}
+            type='checkbox'
+            onChange={updateCheckBox}
+            checked={isChecked}
+          />
+          <span ref={todoSpan}>{todo}</span>
         </label>
+        <button data-testid='modify-button'>수정</button>
+        <button onClick={deleteTodo} data-testid='delete-button'>
+          삭제
+        </button>
       </li>
     </>
   );
--- src/pages/TodoListPage.jsx
+++ src/pages/TodoListPage.jsx
@@ -1,10 +1,100 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import TodoList from '../components/TodoList';
 
 export default function TodoListPage() {
+  const todoInput = useRef('');
+  const [todos, setTodos] = useState([]);
+
+  const createTodo = async (e) => {
+    e.preventDefault();
+
+    const todo = {
+      todo: todoInput.current.value,
+    };
+
+    const accessToken = JSON.parse(localStorage.getItem('JWT')).access_token;
+
+    try {
+      const res = await fetch(
+        'https://pre-onboarding-selection-task.shop/todos',
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(todo),
+        }
+      );
+
+      if (res.status === 201) {
+        const data = await res.json();
+        console.log('성공', data);
+      } else {
+        console.log(`요청실패, status는 ${res.status}`);
+      }
+    } catch (err) {
+      console.error(`요청실패, 에러는 ${err}`);
+    }
+
+    // TodoList를 다시 가져오고, input은 비워준다.
+    getTodo();
+    todoInput.current.value = '';
+  };
+
+  const getTodo = async () => {
+    const accessToken = JSON.parse(localStorage.getItem('JWT')).access_token;
+
+    try {
+      const res = await fetch(
+        'https://pre-onboarding-selection-task.shop/todos',
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      if (res.status === 200) {
+        const data = await res.json();
+        console.log('성공', data);
+        setTodos(data);
+      } else {
+        console.log(`요청실패, status는 ${res.status}`);
+      }
+    } catch (err) {
+      console.error(`요청실패, 에러는 ${err}`);
+    }
+  };
+
+  useEffect(() => {
+    // 마운트 시에 TodoList를 가져온다.
+    getTodo();
+  }, []);
+
   return (
     <>
-      <h1>TodoList</h1>
-      
+      <h1>해야할 일</h1>
+      <form onSubmit={createTodo}>
+        <input
+          ref={todoInput}
+          data-testid='new-todo-input'
+          type='text'
+          placeholder='할일을 작성하세요!'
+        />
+        <button data-testid='new-todo-add-button'>추가</button>
+      </form>
+      {todos.map((el) => (
+        <TodoList
+          key={el.id}
+          id={el.id}
+          todo={el.todo}
+          isCompleted={el.isCompleted}
+          setTodos={setTodos}
+          todos={todos}
+        />
+      ))}
     </>
   );
 }

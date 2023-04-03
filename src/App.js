b10d9d180576a47c3b3541f5c02080e7ee1d02a8
JKyEun <j56237@naver.com>
2023-04-03 10:06:58 +0900

    chore: browserRouter 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
@@ -8,14 +8,12 @@ import TodoListPage from './pages/TodoListPage';
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/signup' element={<SignUpPage />} />
-          <Route path='/signin' element={<SignInPage />} />
-          <Route path='/todo' element={<TodoListPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/signin' element={<SignInPage />} />
+        <Route path='/todo' element={<TodoListPage />} />
+      </Routes>
     </div>
   );
 }
